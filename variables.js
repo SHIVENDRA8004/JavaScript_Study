49f8eb737b57f6d6c6aea6ca269b3ac490ec934c
SHIVENDRA8004 <jat13632@gmail.com>
2022-08-20 22:21:45 +0530

    Added about JavaScript variables

---
+++ variables.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')();
+// VAR variable
+var x = 5;
+var y = "Shiva";
+var x = 55;
+var y = "Shivendra";
+console.log(x);
+console.log(y);
+x = "Shivendra";
+y = "8";
+console.log(x);
+console.log(y);
+
+// LET variable
+// let a;
+let a = 5;
+console.log(a);
+a =89;
+console.log(a);
+
+
+// CONST Variable
+
+// const m;
+const m = "Shiva";
+// m = "JavaSc";
+console.log(m);
