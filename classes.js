16129250a0d03ab744eb0e19ee12f3c2b7d550e1
SHIVENDRA8004 <jat13632@gmail.com>
2022-08-20 18:24:51 +0530

    Added about Expression,Getter Setter function, and Methods in Classes.js file

--- classes.js
+++ classes.js
@@ -1,13 +1,77 @@
 const prompt = require('prompt-sync')();
-class Product{
+
+
+
+// This is How we can declare a class in JavaScript
+// class Product{
+//     constructor(item_name,price,discount,product_code){
+//         this.item_name=item_name;
+//         this.price=price;
+//         this.discount=discount;
+//         this.product_code=product_code;
+//     }
+// }
+// let product1 = new Product('Pencil',10,2,'P1');
+// let product2 = new Product('Book',100,5,'P2');
+// console.log(product1.item_name);
+// console.log(product2.item_name)
+
+
+
+// Class Expressions
+// const Product1= class {
+//     constructor(item_name,price,discount,product_code){
+//         this.item_name=item_name;
+//         this.price=price;
+//         this.discount=discount;
+//         this.product_code=product_code;
+//     }
+// }
+// let chair=new Product1('Chair',499,15,'C10');
+// console.log(Product1.name);
+
+
+// Getter and Setter function in Classes
+
+// const Product1= class {
+//     constructor(item_name,price,discount,product_code){
+//         this.item_name=item_name;
+//         this.price=price;
+//         this.discount=discount;
+//         this.product_code=product_code;
+//     }
+//     get getDiscountValue(){
+//         return this.discount;
+//     }
+//     set setDiscountValue(value){
+//         this.discount=value;
+//     }
+// }
+// let chair=new Product1('Chair',499,15,'C10');
+// console.log(chair.getDiscountValue);
+// chair.setDiscountValue=10
+// console.log(chair.getDiscountValue);
+
+
+
+//  Methods in Classes
+
+const Product1= class {
     constructor(item_name,price,discount,product_code){
         this.item_name=item_name;
         this.price=price;
         this.discount=discount;
         this.product_code=product_code;
     }
+    get getDiscountValue(){
+        return this.discount;
+    }
+    set setDiscountValue(value){
+        this.discount=value;
+    }
+    discountValue(){
+        return this.discount*this.price/100;
+    };
 }
-let product1 = new Product('Pencil',10,2,'P1');
-let product2 = new Product('Book',100,5,'P2');
-console.log(product1.item_name);
-console.log(product2.item_name)
+let chair=new Product1('Chair',499,15,'C10');
+console.log(chair.discountValue());
