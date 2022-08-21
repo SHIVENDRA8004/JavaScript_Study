7715e9fb595e60440a390a2e736e270ca109cb5d
SHIVENDRA8004 <jat13632@gmail.com>
2022-08-21 15:05:05 +0530

    Added files of JavaScript Data Types

---
+++ dataTypes.js
@@ -0,0 +1,14 @@
+var x = 25;
+console.log(typeof x);
+var y= "Hello";
+console.log(typeof y);
+var z = true;
+console.log(typeof z);
+var a = ["Shiv","Shivendra","Jat","201B256"];
+console.log(typeof a);
+var b  = {first:"Shivendra",last:"Jat",class:"BY"}
+console.log(typeof b);
+var c = null;
+console.log(typeof c);
+var d;
+console.log(typeof d);
